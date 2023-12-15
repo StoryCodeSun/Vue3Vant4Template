e2c996b823a24cfcf8d5f44592230db671e0907e
安生 <story0809@163.com>
2023-12-15 22:38:39 +0800

    update code

--- src/service/index.js
+++ src/service/index.js
@@ -1,6 +1,7 @@
 import { getSto } from "@/utils/general/index.js";
 import { BASE_URL, BASE_API, TIME_OUT } from "./config";
 import request from "./request";
+const MODE = import.meta.env.MODE;
 // 后端解决跨域 BASE_API | http
 // 前端代理跨域 BASE_URL | 相对路径 /api
 const beginRequest = new request({
@@ -16,6 +17,9 @@ const beginRequest = new request({
       return config;
     },
     responseSuccessFn: (response) => {
+      if (MODE == "development" && response.data?.code != 200) {
+        alert("后端接口出错啦-_-||༼ つ ◕_◕ ༽つ==>" + response.data?.msg ?? response.data?.message);
+      }
       // 响应拦截器
       return response.data;
     },
